cf303d14fcf743180b825916efdcf4e661c9e2e5
William Lin <wlin26@yahoo.com>
2017-11-04 03:35:31 -0400

    Updated login java

--- app/src/main/java/me/williamlin/swapbook/login.java
+++ app/src/main/java/me/williamlin/swapbook/login.java
@@ -1,132 +1,69 @@
 package me.williamlin.swapbook;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
-//AWS imports:
-import com.amazonaws.mobile.auth.core.DefaultSignInResultHandler;
-import com.amazonaws.mobile.auth.core.IdentityManager;
-import com.amazonaws.mobile.auth.core.IdentityProvider;
-import com.amazonaws.mobile.auth.ui.AuthUIConfiguration;
-import com.amazonaws.mobile.auth.ui.SignInActivity;
-import com.amazonaws.mobile.config.AWSConfiguration;
-import com.amazonaws.mobile.auth.userpools.CognitoUserPoolsSignInProvider;
-
-import com.amazonaws.mobile.auth.core.DefaultSignInResultHandler;
-import com.amazonaws.mobile.auth.core.IdentityManager;
-import com.amazonaws.mobile.auth.core.IdentityProvider;
-import com.amazonaws.mobile.auth.core.StartupAuthErrorDetails;
-import com.amazonaws.mobile.auth.core.StartupAuthResult;
-import com.amazonaws.mobile.auth.core.StartupAuthResultHandler;
-import com.amazonaws.mobile.auth.core.signin.AuthException;
-import com.amazonaws.mobile.auth.ui.AuthUIConfiguration;
-import com.amazonaws.mobile.auth.ui.SignInActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
-        final IdentityManager identityManager =
-                IdentityManager.getDefaultIdentityManager();
-
-        identityManager.doStartupAuth(this,
-                new StartupAuthResultHandler() {
-                    @Override
-                    public void onComplete(final StartupAuthResult authResults) {
-                        if (authResults.isUserSignedIn()) {
-                            final IdentityProvider provider =
-                                    identityManager.getCurrentIdentityProvider();
-
-                            // If the user was  signed in previously with a provider,
-                            // indicate that to them with a toast.
-                            Toast.makeText(
-                                    login.this, String.format("Signed in with %s",
-                                            provider.getDisplayName()), Toast.LENGTH_LONG).show();
-                            goMain(login.this);
-                            return;
-
-                        } else {
-                            // Either the user has never signed in with a provider before
-                            // or refresh failed with a previously signed in provider.
-
-                            // Optionally, you may want to check if refresh
-                            // failed for the previously signed in provider.
-
-                            final StartupAuthErrorDetails errors =
-                                    authResults.getErrorDetails();
-
-                            if (errors.didErrorOccurRefreshingProvider()) {
-                                final AuthException providerAuthException =
-                                        errors.getProviderRefreshException();
-
-                                // Credentials for previously signed-in provider could not be refreshed
-                                // The identity provider name is available here using:
-                                //     providerAuthException.getProvider().getDisplayName()
-
-                            }
-
-                            doSignIn(IdentityManager.getDefaultIdentityManager());
-                            return;
-                        }
-
-
-                    }
-                }, 2000);
+        mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_login);
     }
 
-    private void doSignIn(final IdentityManager identityManager) {
+    @Override
+    public void onStart(){
+        super.onStart();
 
-        identityManager.setUpToAuthenticate(
-                login.this, new DefaultSignInResultHandler() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user != null) goToMain();
+    }
 
+    public void loginUser(View view){
+        EditText emailTxt = (EditText)findViewById(R.id.input_email);
+        EditText pwTxt = (EditText)findViewById(R.id.input_password);
+        String email = emailTxt.getText().toString();
+        String password = pwTxt.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onSuccess(Activity activity, IdentityProvider identityProvider) {
-                        if (identityProvider != null) {
-
-                            // Sign-in succeeded
-                            // The identity provider name is available here using:
-                            //     identityProvider.getDisplayName()
-
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("LOGIN", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goToMain();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("LOGIN", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //Login Failed
                         }
 
-                        // On Success of SignIn go to your startup activity
-                        activity.startActivity(new Intent(activity, Splash.class)
-                                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                    }
-
-                    @Override
-                    public boolean onCancel(Activity activity) {
-                        // Return false to prevent the user from dismissing
-                        // the sign in screen by pressing back button.
-                        // Return true to allow this.
-
-                        return false;
+                        // ...
                     }
                 });
-
-        AuthUIConfiguration config =
-                new AuthUIConfiguration.Builder()
-                        .userPools(true)
-                        // .signInButton(FacebookButton.class)
-                        // .signInButton(GoogleButton.class)
-                        .build();
-
-        Context context = login.this;
-        SignInActivity.startSignInActivity(context, config);
-        login.this.finish();
     }
 
-    /** Go to the main activity. */
-    private void goMain(final Activity callingActivity) {
-        callingActivity.startActivity(new Intent(callingActivity, Splash.class)
-                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-        callingActivity.finish();
+    private void goToMain(){
+        if(mAuth.getCurrentUser() != null){
+            //Intent to main
+        }
     }
 }
