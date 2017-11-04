3f064b2d8061793db6b291d5257693d1c2b885f0
William Lin <wlin26@yahoo.com>
2017-11-04 03:35:12 -0400

    Integrated Firebase Auth

--- app/src/main/java/me/williamlin/swapbook/Splash.java
+++ app/src/main/java/me/williamlin/swapbook/Splash.java
@@ -1,10 +1,19 @@
 package me.williamlin.swapbook;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Space;
+import android.widget.Toast;
 
+import com.amazonaws.mobile.auth.core.DefaultSignInResultHandler;
+import com.amazonaws.mobile.auth.core.IdentityProvider;
+import com.amazonaws.mobile.auth.core.StartupAuthErrorDetails;
+import com.amazonaws.mobile.auth.core.signin.AuthException;
+import com.amazonaws.mobile.auth.ui.AuthUIConfiguration;
+import com.amazonaws.mobile.auth.ui.SignInActivity;
 import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobile.auth.core.IdentityManager;
 import com.amazonaws.mobile.auth.core.StartupAuthResultHandler;
@@ -15,31 +24,116 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initializeApplication();
         setContentView(R.layout.activity_splash);
 
-        Context appContext = getApplicationContext();
-        AWSConfiguration awsConfig = new AWSConfiguration(appContext);
-        IdentityManager identityManager = new IdentityManager(appContext, awsConfig);
-        IdentityManager.setDefaultIdentityManager(identityManager);
-        identityManager.doStartupAuth(this, new StartupAuthResultHandler() {
-            @Override
-            public void onComplete(StartupAuthResult startupAuthResult) {
-                if(startupAuthResult.isUserSignedIn()){
-                    //User is signed in
-                }
-                if(!startupAuthResult.isUserSignedIn()) goToLogin();
-            }
-        });
+        final IdentityManager identityManager =
+                IdentityManager.getDefaultIdentityManager();
 
-        goToLogin();
+        identityManager.doStartupAuth(Splash.this,
+                new StartupAuthResultHandler() {
+                    @Override
+                    public void onComplete(final StartupAuthResult authResults) {
+                        if (authResults.isUserSignedIn()) {
+                            final IdentityProvider provider =
+                                    identityManager.getCurrentIdentityProvider();
+
+                            // If the user was  signed in previously with a provider,
+                            // indicate that to them with a toast.
+                            Toast.makeText(
+                                    Splash.this, String.format("Signed in with %s",
+                                            provider.getDisplayName()), Toast.LENGTH_LONG).show();
+                            goMain(Splash.this);
+                            return;
+
+                        } else {
+                            // Either the user has never signed in with a provider before
+                            // or refresh failed with a previously signed in provider.
+
+                            // Optionally, you may want to check if refresh
+                            // failed for the previously signed in provider.
+
+                            final StartupAuthErrorDetails errors =
+                                    authResults.getErrorDetails();
+
+                            if (errors.didErrorOccurRefreshingProvider()) {
+                                final AuthException providerAuthException =
+                                        errors.getProviderRefreshException();
+
+                                // Credentials for previously signed-in provider could not be refreshed
+                                // The identity provider name is available here using:
+                                //     providerAuthException.getProvider().getDisplayName()
+
+                            }
+
+                            doSignIn(IdentityManager.getDefaultIdentityManager());
+                            return;
+                        }
+
+
+                    }
+                }, 2000);
+    }
+
+    private void initializeApplication() {
+
+        AWSConfiguration awsConfiguration = new AWSConfiguration(getApplicationContext());
+
+        // If IdentityManager is not created, create it
+        if (IdentityManager.getDefaultIdentityManager() == null) {
+            IdentityManager identityManager =
+                    new IdentityManager(getApplicationContext(), awsConfiguration);
+            IdentityManager.setDefaultIdentityManager(identityManager);
+        }
 
     }
 
-    private void goToLogin(){
-        // Go to the login Activity
-        final Intent intent = new Intent(this, login.class)
-                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        this.startActivity(intent);
-        this.finish();
+    private void doSignIn(final IdentityManager identityManager) {
+
+        identityManager.setUpToAuthenticate(
+                Splash.this, new DefaultSignInResultHandler() {
+
+                    @Override
+                    public void onSuccess(Activity activity, IdentityProvider identityProvider) {
+                        if (identityProvider != null) {
+
+                            // Sign-in succeeded
+                            // The identity provider name is available here using:
+                            //     identityProvider.getDisplayName()
+
+                        }
+
+                        // On Success of SignIn go to your startup activity
+                        activity.startActivity(new Intent(activity, Splash.class)
+                                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                    }
+
+                    @Override
+                    public boolean onCancel(Activity activity) {
+                        // Return false to prevent the user from dismissing
+                        // the sign in screen by pressing back button.
+                        // Return true to allow this.
+
+                        return false;
+                    }
+                });
+
+        AuthUIConfiguration config =
+                new AuthUIConfiguration.Builder()
+                        .userPools(true)
+                        // .signInButton(FacebookButton.class)
+                        // .signInButton(GoogleButton.class)
+                        .build();
+
+        Context context = Splash.this;
+        SignInActivity.startSignInActivity(context, config);
+        Splash.this.finish();
+    }
+
+    /** Go to the main activity. */
+    private void goMain(final Activity callingActivity) {
+        callingActivity.startActivity(new Intent(callingActivity, Splash.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+        callingActivity.finish();
     }
 }
--- app/src/main/java/me/williamlin/swapbook/login.java
+++ app/src/main/java/me/williamlin/swapbook/login.java
@@ -1,40 +1,132 @@
 package me.williamlin.swapbook;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 //AWS imports:
+import com.amazonaws.mobile.auth.core.DefaultSignInResultHandler;
 import com.amazonaws.mobile.auth.core.IdentityManager;
+import com.amazonaws.mobile.auth.core.IdentityProvider;
 import com.amazonaws.mobile.auth.ui.AuthUIConfiguration;
 import com.amazonaws.mobile.auth.ui.SignInActivity;
 import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobile.auth.userpools.CognitoUserPoolsSignInProvider;
+
+import com.amazonaws.mobile.auth.core.DefaultSignInResultHandler;
+import com.amazonaws.mobile.auth.core.IdentityManager;
+import com.amazonaws.mobile.auth.core.IdentityProvider;
+import com.amazonaws.mobile.auth.core.StartupAuthErrorDetails;
+import com.amazonaws.mobile.auth.core.StartupAuthResult;
+import com.amazonaws.mobile.auth.core.StartupAuthResultHandler;
+import com.amazonaws.mobile.auth.core.signin.AuthException;
+import com.amazonaws.mobile.auth.ui.AuthUIConfiguration;
+import com.amazonaws.mobile.auth.ui.SignInActivity;
 
 public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initializeApplication();
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_splash);
+
+        final IdentityManager identityManager =
+                IdentityManager.getDefaultIdentityManager();
+
+        identityManager.doStartupAuth(this,
+                new StartupAuthResultHandler() {
+                    @Override
+                    public void onComplete(final StartupAuthResult authResults) {
+                        if (authResults.isUserSignedIn()) {
+                            final IdentityProvider provider =
+                                    identityManager.getCurrentIdentityProvider();
+
+                            // If the user was  signed in previously with a provider,
+                            // indicate that to them with a toast.
+                            Toast.makeText(
+                                    login.this, String.format("Signed in with %s",
+                                            provider.getDisplayName()), Toast.LENGTH_LONG).show();
+                            goMain(login.this);
+                            return;
+
+                        } else {
+                            // Either the user has never signed in with a provider before
+                            // or refresh failed with a previously signed in provider.
+
+                            // Optionally, you may want to check if refresh
+                            // failed for the previously signed in provider.
+
+                            final StartupAuthErrorDetails errors =
+                                    authResults.getErrorDetails();
+
+                            if (errors.didErrorOccurRefreshingProvider()) {
+                                final AuthException providerAuthException =
+                                        errors.getProviderRefreshException();
+
+                                // Credentials for previously signed-in provider could not be refreshed
+                                // The identity provider name is available here using:
+                                //     providerAuthException.getProvider().getDisplayName()
+
+                            }
+
+                            doSignIn(IdentityManager.getDefaultIdentityManager());
+                            return;
+                        }
+
+
+                    }
+                }, 2000);
     }
 
-    private void initializeApplication() {
+    private void doSignIn(final IdentityManager identityManager) {
 
-        AWSConfiguration awsConfiguration = new AWSConfiguration(getApplicationContext());
+        identityManager.setUpToAuthenticate(
+                login.this, new DefaultSignInResultHandler() {
 
-        // If IdentityManager is not created, create it
-        if (IdentityManager.getDefaultIdentityManager() == null) {
-            IdentityManager identityManager =
-                    new IdentityManager(getApplicationContext(), awsConfiguration);
-            IdentityManager.setDefaultIdentityManager(identityManager);
-        }
+                    @Override
+                    public void onSuccess(Activity activity, IdentityProvider identityProvider) {
+                        if (identityProvider != null) {
 
-        // Add Amazon Cognito User Pools as Identity Provider.
-        IdentityManager.getDefaultIdentityManager().addSignInProvider(
-                CognitoUserPoolsSignInProvider.class);
+                            // Sign-in succeeded
+                            // The identity provider name is available here using:
+                            //     identityProvider.getDisplayName()
 
-        // . . .
+                        }
 
+                        // On Success of SignIn go to your startup activity
+                        activity.startActivity(new Intent(activity, Splash.class)
+                                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                    }
+
+                    @Override
+                    public boolean onCancel(Activity activity) {
+                        // Return false to prevent the user from dismissing
+                        // the sign in screen by pressing back button.
+                        // Return true to allow this.
+
+                        return false;
+                    }
+                });
+
+        AuthUIConfiguration config =
+                new AuthUIConfiguration.Builder()
+                        .userPools(true)
+                        // .signInButton(FacebookButton.class)
+                        // .signInButton(GoogleButton.class)
+                        .build();
+
+        Context context = login.this;
+        SignInActivity.startSignInActivity(context, config);
+        login.this.finish();
+    }
+
+    /** Go to the main activity. */
+    private void goMain(final Activity callingActivity) {
+        callingActivity.startActivity(new Intent(callingActivity, Splash.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+        callingActivity.finish();
     }
 }
