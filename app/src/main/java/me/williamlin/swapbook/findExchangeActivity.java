c80ad4f744f0faeb2e88ac125fede67030030b8a
William Lin <wlin26@yahoo.com>
2017-11-04 23:27:05 -0400

    Fresh new updates for exchange
    Will work on it more to make it visual and not in the LogCat

--- app/src/main/java/me/williamlin/swapbook/findExchangeActivity.java
+++ app/src/main/java/me/williamlin/swapbook/findExchangeActivity.java
@@ -7,28 +7,45 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.common.collect.ArrayTable;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.Console;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class findExchangeActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     FirebaseUser currentUser;
     FirebaseFirestore db;
+
+    CollectionReference bookRef;
+    CollectionReference userRef;
+
+    List<String> everyUID = new ArrayList<>();
+    List<String> allYourNeeds = new ArrayList<>();
+
+    Map<String, String> matches = new HashMap<>();
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        try{
+            this.wait(1300);
+        } catch (Exception ex){
+            Log.e("Error", ex.getMessage());
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,26 +55,67 @@ public class findExchangeActivity extends AppCompatActivity {
         currentUser = mAuth.getCurrentUser();
 
         db = FirebaseFirestore.getInstance();
+        bookRef = db.collection("books");
+        userRef = db.collection("users");
 
-        findMatches(getCurrentFocus());
-        setContentView(R.layout.activity_find_exchange);
-    }
-
-    public void findMatches(View view){
-
-        Query match = db.collection("users").document("wtz5jtb3hcT9R8U9TmxBz5UrnHy1").collection("haveBooks").whereEqualTo("ISBN", "12345678");
-        Task<QuerySnapshot> snap = match.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        bookRef.get().addOnSuccessListener(this, new OnSuccessListener<QuerySnapshot>() {
             @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                List<DocumentSnapshot> data = task.getResult().getDocuments();
-                Log.d("Test", "test");
+            public void onSuccess(QuerySnapshot documentSnapshots) {
+                for(DocumentSnapshot d : documentSnapshots.getDocuments()){
+                    Log.d("Test", d.toString());
+                }
             }
         });
 
-        Log.d("Test", "test");
+
+        userRef.document(currentUser.getUid()).collection("wantedBooks").get()
+                .addOnSuccessListener(this, new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot documentSnapshots) {
+                        for(DocumentSnapshot ds : documentSnapshots.getDocuments()){
+                            if(ds.exists()){
+                                Log.d("Your Need", "One of your needs: " + ds.get("ISBN").toString());
+                                allYourNeedsAdd(ds.get("ISBN").toString());
+                            }
+                        }
+                    }
+                });
+
+        userRef.get().addOnSuccessListener(this, new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot documentSnapshots) {
+                for(DocumentSnapshot d : documentSnapshots.getDocuments()){
+                    if(d.exists()){
+                        Log.d("Tester", d.getId());
+                        if(!d.getId().equals(currentUser.getUid())) everyUIDAdd(d.getId());
+                    }
+                }
+                findMyNeeds();
+                setContentView(R.layout.activity_find_exchange);
+            }
+        });
     }
 
-    public String[] getUsers(String university){
-        return null;
+    public void findMyNeeds(){
+        final int count = 0;
+        for(final String uid : everyUID){
+            userRef.document(uid).collection("haveBooks").get().addOnSuccessListener(this, new OnSuccessListener<QuerySnapshot>() {
+                @Override
+                public void onSuccess(QuerySnapshot documentSnapshots) {
+                    for(DocumentSnapshot ds : documentSnapshots.getDocuments()){
+                        Log.d("HAHAHAHAHAHAHAH", ds.get("ISBN").toString());
+                        if(allYourNeeds.contains(ds.get("ISBN"))){
+                            matches.put(uid, ds.get("ISBN").toString());
+                        }
+                    }
+
+                    Log.d("MAMAMAMAMAM", "Size of Match: " + matches.size());
+                }
+            });
+        }
+
     }
+
+    public void everyUIDAdd(String uid){ everyUID.add(uid); }
+    public void allYourNeedsAdd(String need){ allYourNeeds.add(need); }
 }
